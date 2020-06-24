c1b738be45609aa01938cbfe1fdedf1a8da398be
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2020-06-24 18:58:11 +0300

    Add icon ToolBar

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import android.widget.Toolbar
 import com.example.chat.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
@@ -10,6 +12,7 @@ import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 
 class MainActivity : AppCompatActivity() {
@@ -46,9 +49,59 @@ class MainActivity : AppCompatActivity() {
             .addDrawerItems(
                 PrimaryDrawerItem().withIdentifier(100)
                     .withIconTintingEnabled(true)
+                    .withName("Invire")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_add_user),
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
                     .withName("Create group")
                     .withSelectable(false)
-            ).build()
+                    .withIcon(R.drawable.icon_menu_create_group),
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Favorites")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_favorites),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Find")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_find),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Invite")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_invite),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Phone")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_phone),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Secret chat")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_secret_chat),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Settings")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_settings),
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName("Help")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.icon_menu_help)
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(applicationContext,position.toString(),Toast.LENGTH_SHORT).show()
+                    return false
+                }
+            }).build()
     }
 
     private fun createHeader() {
