fcddb55875af5f915e944534963daa6a44f66515
bogoslovskiydenis <bogoslovskiydenis@gmail.com>
2020-06-28 12:24:46 +0300

    Create ChatFragment . SettingsFragment

---
+++ app/src/main/java/com/example/chat/ui/ChatFragment.kt
@@ -0,0 +1,26 @@
+package com.example.chat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chat.R
+import com.example.chat.databinding.FragmentChatBinding
+
+
+class ChatFragment : Fragment() {
+private lateinit var mBinding: FragmentChatBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentChatBinding.inflate(layoutInflater)
+        return mBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/example/chat/ui/SettingsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.chat.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chat.R
+import com.example.chat.databinding.FragmentChatBinding
+import com.example.chat.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentSettingsBinding.inflate(layoutInflater)
+        return mBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
